0e6df59246425a86c3b72c8ba9a0657348c49706
paveytel <paveytel@buffalo.edu>
2015-01-11 13:57:51 -0500

    created FeedAdapter.java and listview_layout.java to adapt data into the view

---
+++ app/src/main/java/FeedAdapter.java
@@ -0,0 +1,13 @@
+import android.content.Context;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by paveynganpi on 1/11/15.
+ */
+public class FeedAdapter extends ArrayAdapter{
+
+    public FeedAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+}
